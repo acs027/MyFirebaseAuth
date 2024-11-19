53ad9604e854815ecf47b0e33ec7467bb3b3fade
acs027 <alicihansarac@gmail.com>
2024-11-19 18:08:21 +0300

    Initial Commit

---
+++ MyFirebaseAuth/MyFirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirebaseAuthApp.swift
+//  MyFirebaseAuth
+//
+//  Created by ali cihan on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
